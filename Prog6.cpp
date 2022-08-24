23f8b4cb173982febd544194930a230efd8e3265
YeahimBrody <brodygpennington@gmail.com>
2022-08-24 08:50:52 -0500

    Prog6

---
+++ Prog6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+
+
+int main() {
+
+    double total= 2500.00 ;
+     
+    float y = 0.04 ;
+    int i ;
+    
+    std::cout << std::setprecision(2) << std::fixed;
+
+    for (i = 1 ; i <= 6 ; i++)
+    {
+        
+       total = total + (total * y) ;
+   
+       std::cout<< "Year " << i << " rate: " <<"$" << total << std::endl;
+    }
+
+    system("pause");
+    return 0;
+}
