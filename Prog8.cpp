0f43ebcf583451881f7914a7e0139846c2d24475
Yeahimbrody <Brodygpennington@gmail.com>
2022-09-02 11:07:20 -0500

    prog 8

--- Prog8.cpp
+++ Prog8.cpp
@@ -10,17 +10,27 @@ int main() {
     std::cout << "What is your first and last name?" << std::endl;
     std::cin >> first >> last;
 
-    std::ofstream MyFile(first + "_" + last + ".txt");
 
     std::cout << "Now write a brief sentence that describes yourself!" << std::endl;
     std::cin >> sentence;
 
-    MyFile.open (first + "_" + last + ".txt");
+    std::ofstream MyFile(first + "_" + last + ".html");
+
     MyFile << "<!DOCTYPE html>";
     MyFile << "<html>";
-    MyFile << "<head>" + first + last + "'s webpage" + "</head>";
+    MyFile << "<head>" + first + " " + last + "'s webpage" + "</head>";
     MyFile << "<body>";
     MyFile << " <center>";
-    MyFile << "     <h1>" + first + last + "</h1>";
+    MyFile << "<h1>" + first + " " + last + "</h1>";
     MyFile << " </center>";
-    MyFile << " hr /";
+    MyFile << "<hr>";
+    MyFile << " <center>";
+    MyFile << sentence;
+    MyFile << " </center>";
+    MyFile << "</body>";
+    MyFile << "</html>";
+    MyFile.close();
+
+    system("pause");
+    return 0;
+}
