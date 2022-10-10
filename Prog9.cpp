04f12c6807aeec2d91ae35d9bce2336324ba0366
YeahimBrody <brodygpennington@gmail.com>
2022-10-09 21:45:32 -0500

    prog9

--- Prog9.cpp
+++ Prog9.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <stdlib.h>
 
-int (main) {
+using namespace std;
 
-    std::cout << ""
+double calculateRetail(double cost, double percent) {
+	double totalCost = 0;
+	totalCost = ((cost / 100) * percent) + cost;
+	return totalCost;
+}
 
+int main() {
+
+	double wholeSaleCost = 0;
+	double percentage = 0;
+	double totalPrice = 0;
+
+	std::cout << "Please enter non negative value for the wholesale price and the percentage:" << std::endl;
+	std::cin >> wholeSaleCost >> percentage;
+	while (wholeSaleCost < 0 || percentage < 0) {
+		std::cout << "Please enter non negative value for the wholesale price and the percentage:" << std::endl;
+		std::cin >> wholeSaleCost >> percentage;
+	}
+
+	totalPrice = calculateRetail(wholeSaleCost, percentage);
+	std::cout << "The total cost is : $" << totalPrice << std::endl; 
+
+
+    system("pause");
+    return 0;
 }
